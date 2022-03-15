17022e580fba2819ad16e9221327c10ededf77d1
haya-y <89266429+haya-y@users.noreply.github.com>
2022-03-16 03:47:48 +0900

    [Add] atomic-designの練習

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((v) => {
+  return {
+    id: v,
+    name: `じゃけぇ${v}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "udemy@example.com",
+    phone: "111-2222-3333",
+    company: {
+      name: "Udemy",
+      website: "https://google.com"
+    }
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
